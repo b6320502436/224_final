fc69d315c7ea45cb0484981ee40beaa407a5d8f4
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-03-29 20:28:34 +0800

    Update 224_3.cpp

--- 224_3.cpp
+++ 224_3.cpp
@@ -5,35 +5,35 @@ int  main()
 	scanf("%d %d %d",&a,&b,&c);
 	int A[a][a]={},B[b][b]={},C[c][c]={};
 	int D[2*a+2]={},E[2*b+2]={},F[2*c+2]={};
-	int e,f,g,h=0,i,j,k;
+	int e,f,g,h=0,i,j,k,I[3]={};
+	char x;
 	for(e=0;e<a;e++)
 	{
 		for(f=0;f<a;f++)
 	{
 		scanf("%d",&A[e][f]);
 		D[e]=A[e][f]+D[e];
-		D[f+3]=A[e][f]+D[f+3];
+		D[f+a]=A[e][f]+D[f+a];
 		if(e==f)
 		D[2*a]=D[2*a]+A[e][f];
 		if(e+f==a-1)
 		{D[2*a+1]=D[2*a+1]+A[e][f];
-		printf("-%d-",A[e][f]);
+	//	printf("-%d-",A[e][f]);
 		}
 	}
 }
-for(g=0;g<=2*a+2;g++)
+for(g=0;g<=2*a+1;g++)
 {
 	if(D[0]!=D[g]||D[g]==0)
 	{
 		h=1;
 		
-	}printf("%d ",D[g]);
+	}//printf("%d ",D[g]);
 }
-if(h==0)
+if(h==1)
 {
-	printf("yes\n");
+	I[0]=1;
 }
-else printf("no\n");
 
 	for(e=0;e<b;e++)
 	{
@@ -41,41 +41,60 @@ else printf("no\n");
 	{
 		scanf("%d",&B[e][f]);
 		E[e]=B[e][f]+E[e];
-		E[f+3]=B[e][f]+E[f+3];
+		E[f+b]=B[e][f]+E[f+b];
 		if(e==f)
-		E[2*a+1]=E[2*a+1]+B[e][f];
-		if(e+f==a)
-		E[2*a+2]=E[2*a+1]+B[e][f];
+		E[2*b]=E[2*b]+B[e][f];
+		if(e+f==b-1)
+		E[2*b+1]=E[2*b+1]+B[e][f];
 	}
 }
 h=0;
-for(g=0;g<=2*b+2;g++)
+for(g=0;g<=2*b+1;g++)
 {
-	if(E[0]!=E[g])
+	if(E[0]!=E[g]||E[g]==0)
 	{
 		h=1;
-	}
+		
+	}//printf("%d ",E[g]);
 }
-if(h==0)
+if(h==1)
 {
-	printf("yes\n");
+	I[1]=1;
 }
-else printf("no\n");
-
 		for(e=0;e<c;e++)
 	{
 		for(f=0;f<c;f++)
 	{
-		scanf("%d",&B[e][f]);
+		scanf(" %d",&C[e][f]);
 		F[e]=C[e][f]+F[e];
-		F[f+3]=C[e][f]+F[f+3];
+		F[f+c]=C[e][f]+F[f+c];
 		if(e==f)
-		F[2*a+1]=F[2*a+1]+C[e][f];
-		if(e+f==a)
-		F[2*a+2]=F[2*a+1]+C[e][f];
-	}
+		F[2*c]=F[2*c]+C[e][f];
+		if(e+f==c-1)
+		F[2*c+1]=F[2*c+1]+C[e][f];
+	}}
+h=0;
+for(g=0;g<=2*c+1;g++)
+{
+	if(F[0]!=F[g]||F[g]==0)
+	{
+		h=1;
+		
+	}//printf("%d ",F[g]);
 }
-	
-	
-	
+if(h==1)
+{
+	I[2]=1;
 }
+for(g=0;g<3;g++)
+{
+	if(I[g]==0)
+{
+	printf("no\n");
+}
+else printf("yes\n");
+}
+return 0;
+
+}
+
